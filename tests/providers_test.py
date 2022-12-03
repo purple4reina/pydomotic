c6dab892558a6ebb09dc8bad64a14f4c1fe97fa2
Rey Abolofia <purple4reina@gmail.com>
2022-12-03 00:26:05 -0800

    Remove interface tests.

--- tests/providers_test.py
+++ tests/providers_test.py
@@ -1,38 +1,6 @@
-import pytest
-
 from automaton.providers.fujitsu import FujitsuProvider, FujitsuDevice
 from automaton.providers.gosund import GosundProvider, GosundDevice
 from automaton.providers.noop import NoopProvider, NoopDevice
-
-_test_provider_interface = (
-        FujitsuProvider,
-        GosundProvider,
-        NoopProvider,
-)
-_test_provider_interface_attrs = (
-        'get_device',
-)
-
-@pytest.mark.parametrize('provider', _test_provider_interface)
-def test_provider_interface(provider):
-    for attr in _test_provider_interface_attrs:
-        assert hasattr(provider, attr), f'provider requires attr {attr}'
-
-_test_device_interface = (
-        FujitsuDevice,
-        GosundDevice,
-        NoopDevice,
-)
-_test_device_interface_attrs = (
-        'turn_on',
-        'turn_off',
-        'switch',
-)
-
-@pytest.mark.parametrize('device', _test_device_interface)
-def test_device_interface(device):
-    for attr in _test_device_interface_attrs:
-        assert hasattr(device, attr), f'device requires attr {attr}'
 
 def test_fujitsu_provider(patch_fujitsu):
     patch_provider = patch_fujitsu.provider
